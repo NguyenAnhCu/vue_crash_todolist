869cee6162ca69614c26f93e6bf4ce4f78b41d07
NGUYENANH CU <cnanh1996@gmail.com>
2020-02-07 13:58:31 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <Header />
+    <AddTodo v-on:add-todo ="addTodo"/>
+    <Todos v-bind:todos = "todos" v-on:del-todo="deleteTodo" />
+  </div>
+</template>
+
+<script>
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+
+export default {
+  name: 'app', 
+  components: {
+    Header,
+    Todos,
+    AddTodo
+  }, 
+  data (){ // store todo data
+    return {
+      todos:[]
+    }
+  },
+  methods:{
+    deleteTodo(id){
+      this.todos = this.todos.filter(todos => todos.id !== id);
+    },
+    addTodo(newTodo){
+      this.todos=[...this.todos, newTodo];
+    },
+  }
+}
+</script>
+
+<style>
+  *{
+    box-sizing: border-box;
+    margin: 0px;
+    padding: 0px;
+  }
+  body{
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+
+  .btn{
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
+  }
+</style>
